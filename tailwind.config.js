071e69aed0cfec70d3863722d1bf056ccb9026ab
Luis Fernando Ushiña <guffenix@gmail.com>
2022-12-26 16:16:02 -0500

    feat: enable mundovirtual theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,26 @@ module.exports = {
   plugins: [require('daisyui')],
   daisyui: {
     styled: true,
-    themes: true,
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: '',
-    darkTheme: 'dark',
+    darkTheme: 'mvtheme',
+    themes: [
+      {
+        mvtheme: {
+          primary: '#6419E6',
+          secondary: '#D926A9',
+          accent: '#1FB2A6',
+          neutral: '#0D0D0D',
+          'base-100': '#171717',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
   },
 }
