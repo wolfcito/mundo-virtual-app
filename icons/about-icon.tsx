5be955ba393be3356773bc7b22245d0347ee2cd0
Luis Fernando Ushiña <guffenix@gmail.com>
2022-12-27 13:05:38 -0500

    feat: menu icons added

---
+++ icons/about-icon.tsx
@@ -0,0 +1,19 @@
+export function AboutIcon() {
+  return (
+    <svg
+      className="w-5 h-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      height={20}
+      width={20}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  )
+}
---
+++ icons/menu-icon.tsx
@@ -0,0 +1,19 @@
+export function MenuIcon() {
+  return (
+    <svg
+      className="w-5 h-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      height={20}
+      width={20}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4 6h16M4 12h16M4 18h7"
+      />
+    </svg>
+  )
+}
