816be33b104d57a01e3d124129262a1a17a68d06
Luis Fernando Ushiña <guffenix@gmail.com>
2023-01-04 22:22:32 -0500

    feat: default font app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito } from '@next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nunito.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
