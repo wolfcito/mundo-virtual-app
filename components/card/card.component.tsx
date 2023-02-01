3c246ce1fb37912edfdbd7cad98919f9ae92cfd1
Maria Alejandra Aristizabal <116376693+Maalejandra15@users.noreply.github.com>
2023-02-01 17:23:13 -0500

    docs:documentation,tech stack,coding conventions (#4)
    * docs:documentation,tech stack,coding conventions,resource installation
    
    * docs:documentation,tech stack,coding conventions

---
+++ components/card/card.component.tsx
@@ -0,0 +1,9 @@
+export function Card() {
+  return (
+    <div className="avatar">
+      <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src="https://static2.mujerhoy.com/www/multimedia/201710/30/media/cortadas/17.jpg" />
+      </div>
+    </div>
+  );
+}
