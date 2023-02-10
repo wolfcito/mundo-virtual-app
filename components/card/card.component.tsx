9c561d6160d051ce70993af80444cc5f1158683e
alejandra <alequinterom312@gmail.com>
2023-02-09 23:26:17 -0500

    chore: card 8

--- components/card/card.component.tsx
+++ components/card/card.component.tsx
@@ -1,17 +1,17 @@
 export function Card() {
   return (
-    <div className="flex justify-center mx-24 my-20 bg-black w-300 h-90 rounded-2xl">
-      <div className="mt-4 mb-4 ml-4 mr-4 bg-black w-250 h-80">
+    <div className="flex justify-center mx-24 my-20 bg-black h-90 rounded-2xl xs: w-80 ">
+      <div className="mx-6 mt-4 mb-4 bg-black xs: w-30 h-90 ">
         <img
           className="object-cover w-screen h-full bg-contain rounded-2xl"
           src="https://static.vecteezy.com/system/resources/thumbnails/000/595/331/small/20012019-05.jpg"
         />
       </div>
-      <div className=" bg-black w-50 h-30 top-63.8 left-217.5 items-center mt-14">
-        <h1 className="mb-2 text-4xl text-4xlfont-nunito w-125">
+      <div className=" bg-black  h-30 top-79 left-217.5 items-center  pr-6 xs:w-20 text-sm mt-4">
+        <h1 className="mb-2 font-nunito xs:text-base w-150 ">
           Dribbble Rusia VK Community officially launched!
         </h1>
-        <p className="mb-10 text-lg font-nunito w-100">
+        <p className="mb-10 font-nunito xs:text-lg w-100">
           Send your portfolio to our manager on e-mail and get a chance to be a
           part of our new big dribbblers group
         </p>
