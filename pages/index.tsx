edfe5792ffde9f6166d4a4d148ef4d6bd5831456
alejandra <alequinterom312@gmail.com>
2023-02-21 22:29:05 -0500

    chore: "Working at Card2"

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           ideas through advice, construction prototypes and land them in an MVP.
         </div>
         <Card isExpanded />
-        <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           <Card />
           <Card />
           <Card />
