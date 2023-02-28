81e60ee25de468bc535011e40279dcfe37d5f857
alejandra <alequinterom312@gmail.com>
2023-02-27 23:57:46 -0500

    style:height and margin correction

--- components/card/card.component.tsx
+++ components/card/card.component.tsx
@@ -3,7 +3,7 @@ export function Card({ isExpanded = false }: { isExpanded?: boolean }) {
   return (
     <div
       className={clsx(
-        "flex flex-col mx-auto my-2 bg-black rounded-2xl w-[390px] max-w-md h-[573px]",
+        "flex flex-col mx-auto my-2 bg-black rounded-3xl w-[390px] max-w-md h-[573px]",
         isExpanded
           ? "md:w-[372px] md:max-w-screen-md md:h-[620px] md:my-px lg:w-[320px] lg:max-w-screen-lg lg:h-[573px] lg:my-3 lg:flex-col"
           : "md:w-[750px] md:max-w-screen-md md:mx-auto md:my-20 lg:h-[360px] lg:w-[990px] lg:max-w-screen-lg lg:mx-auto lg:my-18 lg:flex-row"
@@ -11,7 +11,7 @@ export function Card({ isExpanded = false }: { isExpanded?: boolean }) {
     >
       <div
         className={clsx(
-          "mx-2 mt-2 mb-4 bg-black w-[374px] max-w-sm h-72 md:mx-2",
+          "mx-2 mt-2 mb-4 bg-black w-[400px] max-w-sm h-80 md:mx-10",
           isExpanded
             ? "md:w-[355px] md:max-w-screen-md md:h-72 lg:m-2 lg:w-[305px] lg:max-w-screen-lg"
             : "md:w-[740px] md:max-w-screen-md md:h-80 lg:w-[800px] lg:max-w-screen-lg lg:my-5 lg:ml-5 lg:mr-3"
@@ -24,7 +24,7 @@ export function Card({ isExpanded = false }: { isExpanded?: boolean }) {
       </div>
       <div
         className={clsx(
-          "mx-2 mt-4 mb-6 bg-black w-[374px] h-72",
+          "mx-2 mt-4 mb-6 bg-black w-[374px] h-80",
           isExpanded
             ? "md:w-[340px] md:max-w-screen-md lg:w-[300px] lg:max-w-screen-lg lg:m-2"
             : "md:my-6 md:mx-2 md:w-[740px] md:max-w-screen-md  lg:ml-4 lg:mt-14 lg:mr-2"
