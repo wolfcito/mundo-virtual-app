45325b44e0d27011ace486f032a6878036eeef0a
Luis Fernando <guffenix@gmail.com>
2023-04-20 16:47:24 -0500

    chore: cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
     fontFamily: {
-      nunito:["Nunito", "sans-serif"],
+      nunito: ['Nunito', 'sans-serif'],
     },
   },
   plugins: [require('daisyui')],
