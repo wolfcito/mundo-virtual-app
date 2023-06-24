cde762fed37af19fc29c86e42702ecbe5f1f1b6f
Luis Fernando <guffenix@gmail.com>
2023-06-24 18:21:09 -0500

    chore: reorganizing file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,9 @@
-import Link from 'next/link'
-import React from 'react'
-import Particles from './components/particles'
-import { Contact } from './views/contact.view'
-import { Projects } from './views/projects.view'
+import { Contact, Home, Projects } from '~/app/views'
 
-const navigation = [
-  { name: 'Projects', href: '/projects' },
-  { name: 'Contact', href: '/contact' },
-]
-
-export default function Home() {
+export default function HomePage() {
   return (
     <>
-      <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-        {/* <nav className="my-16 animate-fade-in">
-          <ul className="flex items-center justify-center gap-4">
-            {navigation.map((item) => (
-              <Link
-                key={item.href}
-                href={item.href}
-                className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </ul>
-        </nav> */}
-        <div className="animate-glow hidden h-px w-screen animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
-        <Particles
-          className="absolute inset-0 -z-10 animate-fade-in"
-          quantity={100}
-        />
-        <h1 className="z-10 flex animate-title cursor-default whitespace-nowrap bg-white bg-clip-text font-display text-4xl text-transparent duration-1000 sm:text-6xl md:text-9xl ">
-          Mundo<div className="text-purple-600">Virtual</div>
-        </h1>
-
-        <div className="animate-glow hidden h-px w-screen animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
-        <div className="my-16 animate-fade-in text-center">
-          <h2 className="px-10 text-sm text-zinc-500">
-            Transformando ideas en realidad, construyendo juntos una línea de
-            código a la vez.
-          </h2>
-        </div>
-      </div>
+      <Home />
       {/* @ts-expect-error Server Component */}
       <Projects />
       <Contact />
