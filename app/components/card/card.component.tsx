c53ebcab1e0d9dc165fdffec340710436a4792b6
Luis Fernando <guffenix@gmail.com>
2023-06-24 21:59:09 -0500

    chore: card component file structure added

---
+++ app/components/card/card.component.tsx
@@ -0,0 +1,39 @@
+'use client'
+import { motion, useMotionTemplate, useSpring } from 'framer-motion'
+
+import { CardProps } from './card.type'
+
+export function Card({ children }: CardProps) {
+  const mouseX = useSpring(0, { stiffness: 500, damping: 100 })
+  const mouseY = useSpring(0, { stiffness: 500, damping: 100 })
+
+  const onMouseMove = ({ currentTarget, clientX, clientY }: any) => {
+    const { left, top } = currentTarget.getBoundingClientRect()
+    mouseX.set(clientX - left)
+    mouseY.set(clientY - top)
+  }
+
+  const maskImage = useMotionTemplate`radial-gradient(240px at ${mouseX}px ${mouseY}px, white, transparent)`
+  const style = { maskImage, WebkitMaskImage: maskImage }
+
+  return (
+    <div
+      onMouseMove={onMouseMove}
+      className="group relative overflow-hidden rounded-xl border border-zinc-600 duration-700 hover:border-zinc-400/50 hover:bg-zinc-800/10 md:gap-8 "
+    >
+      <div className="pointer-events-none">
+        <div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
+        <motion.div
+          className="absolute inset-0 z-10 bg-gradient-to-br via-zinc-100/10 opacity-100 transition duration-1000 group-hover:opacity-50 "
+          style={style}
+        />
+        <motion.div
+          className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100"
+          style={style}
+        />
+      </div>
+
+      {children}
+    </div>
+  )
+}
