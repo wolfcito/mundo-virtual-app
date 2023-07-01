74a85d7f67d8167a5e4d8eeedea841d4889c7124
Luis Fernando <guffenix@gmail.com>
2023-07-01 03:20:00 -0500

    feat: use correct resources

--- data/products.data.ts
+++ data/products.data.ts
@@ -4,7 +4,7 @@ export const products = [
   {
     slug: 'consultoria-tecnica',
     title: 'Consultoría Técnica',
-    image: '/coin.png',
+    image: '/images/black-elixir-potion.mundovirtual.png',
     description:
       'Acelera el crecimiento de tu software con consultorías técnicas de élite en Mundo Virtual. Expertos en soluciones personalizadas listos para potenciar tu éxito.',
     slogan: '¡Descubre el poder de la consultoría técnica hoy!',
@@ -12,7 +12,7 @@ export const products = [
   {
     slug: 'diseño-prototipos',
     title: 'Diseño de Prototipos',
-    image: '/find.png',
+    image: '/images/gold-elixir-potion.mundovirtual.png',
     description:
       'Descubre el poder del Diseño de Prototipos en Mundo Virtual. Impulsa tu software con interfaces intuitivas y funcionales.',
     slogan: '¡Transforma tus ideas en experiencias digitales impactantes!',
@@ -20,7 +20,7 @@ export const products = [
   {
     slug: 'desarrollo-express',
     title: 'Desarrollo Express',
-    image: '/gift.png',
+    image: '/images/purple-elixir-potion.mundovirtual.png',
     description:
       'Potencia tu software con Desarrollo Express en Mundo Virtual. Obtén soluciones ágiles y eficientes para tus proyectos.',
     slogan: '¡Acelera tu éxito digital con nuestro equipo de expertos!',
