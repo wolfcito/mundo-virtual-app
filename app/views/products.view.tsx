fca626cd62b3b0ce0499fc0c8ac83fe8d6f463da
Luis Fernando <guffenix@gmail.com>
2023-06-30 23:32:16 -0500

    chore: use correct prop for products section

--- app/views/products.view.tsx
+++ app/views/products.view.tsx
@@ -6,17 +6,15 @@ export function Products() {
   return (
     <>
       <div className="animate-glow hidden h-px w-screen animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
-      {/* <div className="bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"> */}
       <div className="container mx-auto flex items-center justify-center px-4">
         <div className="mx-auto mt-32 grid w-full grid-cols-1 gap-8 sm:mt-0 md:grid-cols-2 lg:grid-cols-3">
           {products.map((product) => (
-            <Card key={product.slug} className="hover:border-purple-500">
+            <Card key={product.slug} color="purple">
               <Product project={product} />
             </Card>
           ))}
         </div>
       </div>
-      {/* </div> */}
     </>
   )
 }
